428fdde0a5a3cbffee4f81377e6cb7c5261517cb
Dennis Xiao <dennisboys@gmail.com>
2019-10-06 19:57:57 +1000

    improve header style

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss'
 
-const Header = (props) => {
+const Header = () => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
